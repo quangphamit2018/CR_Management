dd27f9e2115dc7feedf3a6ebea74458594920e64
quangphamit2018 <92535010+quangphamit2018@users.noreply.github.com>
2026-09-18 20:23:58 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -8,8 +8,7 @@
   var CONFIG = {
   "index.html": {
     "url": "https://rnsjiqbbeywgbnqsojce.supabase.co",
-    "key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuc2ppcWJiZXl3Z2JucXNvamNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0OTExNjAsImV4cCI6MjA5MzA2NzE2MH0.1BrlS__GNAMFbBmudl6gUJwJ9j_zoDdAj9rtfhBkhdk"
-  }
+    "key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuc2ppcWJiZXl3Z2JucXNvamNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0OTExNjAsImV4cCI6MjA5MzA2NzE2MH0.1BrlS__GNAMFbBmudl6gUJwJ9j_zoDdAj9rtfhBkhdk" }
 };
 
   function currentFileName() {
